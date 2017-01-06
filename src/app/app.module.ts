a7bb24eb197efbba71ed069ee5afb411f6779457
Gabriel Lucena <gabriel.lucena.toro@gmail.com>
2017-01-06 14:25:00 +0100

    The whole application code. It coded following the tutorial steps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
+/*
+ * app.module.ts
+ */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// battery module
+import { TeslaBatteryModule } from './tesla-battery/tesla-battery.module'
+
+// app module
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +20,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    TeslaBatteryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
